f511134b568261d4e7a44d3d6865a3fd381dec2a
tomo0119shige <tomo0119shige@gmail.com>
2020-07-20 13:17:40 +0900

    prot coreui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './scss/style.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Router>
+          <React.Suspense fallback={loading}>
+            <Switch>
+              <Route exact path="/login" name="Login Page" component={Login} />
+              <Route exact path="/register" name="Register Page" component={Register} />
+            </Switch>
+          </React.Suspense>
+      </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
